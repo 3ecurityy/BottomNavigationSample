9fde395d1bf6b49cd1194a28d91b162a144b4546
MohammadReza.Arabi <3ecurityy@gmail.com>
2020-12-26 23:19:13 +0330

    BottomNavigation Sample

---
+++ app/src/main/java/ir/mohammadrezaarabi/bottomnavigationsample/MainActivity.kt
@@ -0,0 +1,32 @@
+package ir.mohammadrezaarabi.bottomnavigationsample
+
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        val floatingButton = findViewById<FloatingActionButton>(R.id.floating)
+        floatingButton.setOnClickListener {
+            Toast.makeText(applicationContext, "start new activity or fragment :)", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
